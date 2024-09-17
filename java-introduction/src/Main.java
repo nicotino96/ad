f9702d2b199545a78214e07f426dba479b9acb9f
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-09-17 10:34:03 +0200

    Completada tarea 10

--- java-introduction/src/Main.java
+++ java-introduction/src/Main.java
@@ -1,18 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Greeter greeter = new Greeter();
-        greeter.sayHello();
-        Cat cat = new Cat();
-        cat.meow();
-        String miString= greeter.byeWorld();
-        System.out.println(miString);
-        System.out.println(greeter.sumTwoNumbers());
-        SimpleMathDemo simpleMathDemo = new SimpleMathDemo();
-        System.out.println(simpleMathDemo.calculateResult());
-        simpleMathDemo.printMultiplicationInfo();
-        simpleMathDemo.loop50Times();
-        simpleMathDemo.printNumber(5895);
-        simpleMathDemo.printProduct(3,9);
     }
 }
 
