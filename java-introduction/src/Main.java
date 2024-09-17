6a1730bbe425836207f1a292afccde1724ea8cbc
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-09-17 09:09:02 +0200

    Completada tarea 1

--- java-introduction/src/Main.java
+++ java-introduction/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Greeter greeter = new Greeter();
+        greeter.sayHello();
     }
 }
+
