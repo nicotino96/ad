7b6c118f45778d92ccaf3372960af8a48c636539
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-09-17 12:14:34 +0200

    Completada tarea 13

--- java-introduction/src/Main.java
+++ java-introduction/src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         SimpleMathDemo demo = new SimpleMathDemo();
         System.out.println(demo.randomNumberInRange(1,5));
         System.out.println(demo.randomNumberBetween(2,5));
+        System.out.println(demo.getTheSmallerValue(1,2,3,4));
     }
 }
 
--- java-introduction/src/SimpleMathDemo.java
+++ java-introduction/src/SimpleMathDemo.java
@@ -52,6 +52,16 @@ public class SimpleMathDemo {
         int random = (int) (((max-min)*Math.random())+ min);
         return random;
     }
+    public int getTheSmallerValue(int n1, int n2, int n3, int n4){
+        if(n1>n2)
+            n1=n2;
+        if(n1>n3)
+            n1=n3;
+        if(n1>n4)
+            return n4;
+        else
+            return n1;
+    }
 
 
 
