ceebd1b8f8ab5a3393a03c3dfa1c0c4b864690c5
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-09-17 09:20:41 +0200

    Completada tarea 2

---
+++ java-introduction/src/Cat.java
@@ -0,0 +1,5 @@
+public class Cat {
+    public void meow(){
+        System.out.println("miau miu");
+    }
+}
--- java-introduction/src/Main.java
+++ java-introduction/src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
         greeter.sayHello();
+        Cat cat = new Cat();
+        cat.meow();
     }
 }
 
