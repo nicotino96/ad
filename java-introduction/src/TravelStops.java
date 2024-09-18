41d6d4f89c357ac00a845739760d787ccfdb7bec
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-09-18 12:56:48 +0200

    Completada tarea 20

--- java-introduction/src/TravelStops.java
+++ java-introduction/src/TravelStops.java
@@ -16,6 +16,10 @@ public class TravelStops {
     public void printThirdStop() {
         System.out.println(this.stops[2]);
     }
+    public void printStop(int position) {
+        System.out.println(this.stops[position]);
+    }
+
 
 }
 
