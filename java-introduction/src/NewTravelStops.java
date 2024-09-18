d218e90753e0eb91befe213af58f342f63dfb3db
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-09-18 14:01:50 +0200

    Completada tarea 24

---
+++ java-introduction/src/NewTravelStops.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class NewTravelStops {
+    private ArrayList<String> stops;
+    public NewTravelStops(){
+        stops = new ArrayList<>(List.of("Kyoto","Singapur","Hanoi"));
+    }
+    public void printStop(int index){
+        System.out.println(stops.get(index));
+    }
+    public void modifyStop(int index, String newValue){
+        stops.set(index, newValue);
+    }
+    public void addStop(String newValue){
+        stops.add(newValue);
+    }
+}
