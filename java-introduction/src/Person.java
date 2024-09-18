11523ffdb0a06058f2924cb46ac4d29d378d4bb7
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-09-18 12:04:34 +0200

    Completada tarea 16

--- java-introduction/src/Person.java
+++ java-introduction/src/Person.java
@@ -1,8 +1,16 @@
 public class Person {
     private String name;
+    public String getName() {
+        return this.name;
+    }
+
     public Person(String name) {
         this.name=name;
         System.out.println(name+" acaba de ser instanciado");
+
+    }
+    public String greeting() {
+        return "Bienvenido/a, " + this.name;
     }
 }
 
