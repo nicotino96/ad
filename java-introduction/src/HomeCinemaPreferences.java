76e51d3d4da544c004cdc681c44cfbce9c99b52f
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-09-20 13:33:13 +0200

    Tarea 27 completada

--- java-introduction/src/HomeCinemaPreferences.java
+++ java-introduction/src/HomeCinemaPreferences.java
@@ -4,6 +4,25 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class HomeCinemaPreferences {
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public boolean isDarkModePreferred() {
+        return darkModePreferred;
+    }
+
+    public void setDarkModePreferred(boolean darkModePreferred) {
+        this.darkModePreferred = darkModePreferred;
+    }
+
+    private String username;
+    private boolean darkModePreferred;
+
     public HomeCinemaPreferences() throws IOException {
         String file ="assets\\cinemaPrefs.txt";
         try {
@@ -13,6 +32,7 @@ public class HomeCinemaPreferences {
             do {
                 newLine = bufferedReader.readLine();
                 System.out.println(newLine);
+                parseLine(newLine);
             } while (newLine != null);
             reader.close();
 
@@ -21,4 +41,22 @@ public class HomeCinemaPreferences {
             throw new RuntimeException(e);
         }
     }
+    private void parseLine(String line) {
+        if (line == null) {
+            return;
+        }
+        String[] separatedString = line.split("=");
+        String firstHalf = separatedString[0];
+        String secondHalf = separatedString[1];
+        if (firstHalf.equals("username")) {
+            this.username = secondHalf;
+        }
+        if (firstHalf.equals("prefersDarkMode")) {
+            this.darkModePreferred=secondHalf.equals("true");
+        }
+
+
+
+    }
+
 }
