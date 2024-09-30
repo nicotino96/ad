f0a7088297394415aef2f97aef47205b87bc0ca7
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-09-30 13:03:06 +0200

    Tarea 35

---
+++ java-rest/src/MoviesDataProvider.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MoviesDataProvider {
+
+    // Método constructor
+    public MoviesDataProvider() {
+        String connectionStr = "jdbc:sqlite:db/sqlite3/movies.db";
+        try {
+            Connection conn = DriverManager.getConnection(connectionStr);
+            // Se ha establecido la conexión
+            // ...
+            System.out.println("The connection has been established successfully");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
