47b15f3534f9cffc7eed9ffac102d468c39b65ee
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-10-01 09:26:47 +0200

    Tarea 46 terminada

--- java-rest/src/MoviesConnector.java
+++ java-rest/src/MoviesConnector.java
@@ -20,4 +20,19 @@ public class MoviesConnector {
             throw new RuntimeException(e);
         }
     }
+    public void insertExampleFilm() {
+        try {
+            Statement statement = this.connection.createStatement();
+            int affectedRows = statement.executeUpdate("INSERT INTO TMovies (title, year, duration, countryIso3166, genre, synopsis) VALUES ('Inside Out', 2015, 94, 'us', 'animation', 'Riley es una chica que disfruta o padece toda clase de sentimientos. Aunque su vida ha estado marcada por la Alegría, también se ve afectada por otro tipo de emociones. Lo que Riley no entiende muy bien es por qué motivo tiene que existir la Tristeza en su vida. Una serie de acontecimientos hacen que Alegría y Tristeza se mezclen en un peligroso viaje que dará un vuelco al mundo de Riley.')");
+            if (affectedRows == 1) {
+                System.out.println("Se ha insertado satisfactoriamente");
+            } else {
+                System.out.println("Parece que ha habido un problema");
+            }
+            statement.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
