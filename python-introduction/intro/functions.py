c66bb9936b82fd2096693646c9bb305bb561dfb0
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-10-14 13:31:23 +0200

    Tarea 65, más funciones a functions.py

--- python-introduction/intro/functions.py
+++ python-introduction/intro/functions.py
@@ -5,3 +5,7 @@ def do_something():
 def return_something():
     a_number = 47
     return a_number
+def small_print():
+    print("James Watt patentó en 1769 la máquina de vapor")
+def lucky_number():
+    return 63
