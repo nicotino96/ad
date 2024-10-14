719d9b0aaca59b70e2741e178ffbeab7c4f19cb3
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-10-14 13:39:54 +0200

    Tarea 66, big_print a functions.py

--- python-introduction/intro/functions.py
+++ python-introduction/intro/functions.py
@@ -9,3 +9,9 @@ def small_print():
     print("James Watt patentó en 1769 la máquina de vapor")
 def lucky_number():
     return 63
+def big_print():
+    print("""
+    Un hijo le preguntó a su padre (un desarrollador de software)
+    por qué el sol sale por el este y se pone por el oeste. ¿Su 
+    respuesta? ¡Funciona, no lo toques!
+    """)
