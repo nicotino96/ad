669681215fb092ec9a75675d789df33dd5873d58
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-10-15 11:36:11 +0200

    Tarea 80

--- python-introduction/advanced/loops.py
+++ python-introduction/advanced/loops.py
@@ -7,5 +7,9 @@ def simple_for_begin():
 def simple_for_increment():
     for i in range(60, 109, 3):
         print(i)
+def multiples():
+    for i in range(31,57):
+        if i%3==0:
+            print(i)
 if __name__ == '__main__':
     simple_for()
