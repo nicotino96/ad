c9f7d5d2e8d41416630aee6516aa744fe8818417
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-10-15 11:41:58 +0200

    Tarea 81

--- python-introduction/advanced/loops.py
+++ python-introduction/advanced/loops.py
@@ -11,5 +11,8 @@ def multiples():
     for i in range(31,57):
         if i%3==0:
             print(i)
+def multiplication_table(numero):
+    for i in range(1,11):
+        print(numero*i)
 if __name__ == '__main__':
     simple_for()
