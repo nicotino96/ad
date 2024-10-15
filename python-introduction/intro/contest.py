d16d570f2bfc95704b67edc97df0ef1b4e03a961
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-10-15 10:34:47 +0200

    Tarea 76

--- python-introduction/intro/contest.py
+++ python-introduction/intro/contest.py
@@ -15,5 +15,26 @@ c) Universidad de Oxford
         else:
             print("¡Fallaste! Pero no pasa nada, ¡sigue intentándolo!")
 
+def make_question2():
+    question2 = """
+¿Cuál es el resultado de SUMAR estos dos números?
+434
+493
+"""
+    print(question2)
+    answer = None
+    while answer is None:  # Se prefiere 'is' a '==' para comparar con None
+        try:
+            answer = int(input("Dime tu respuesta: "))
+        except ValueError:
+            print("¡No has introducido un número válido!")
+    if answer == (434+493):
+        print("¡Una mente privilegiada para el cálculo!")
+    else:
+        print("Parece que esa respuesta no es correcta, ¡no te rindas!")
+
+
+
 if __name__ == '__main__':
     make_question()
+    make_question2()
