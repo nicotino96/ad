d4227ce63e9192ad13385e8242b32d6634cdbb29
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-10-16 12:46:37 +0200

    tarea 86

--- python-introduction/advanced/chatbot.py
+++ python-introduction/advanced/chatbot.py
@@ -1,3 +1,9 @@
 class ChatBot:
     def test_hello(self):
         print("¡Hola!")
+
+    def test_one_param(self, number):
+        print("Tu número es: " + str(number))
+
+    def substract_numbers(self,number1,number2):
+        print(number1-number2)
--- python-introduction/advanced/run_class_example.py
+++ python-introduction/advanced/run_class_example.py
@@ -3,3 +3,5 @@ from chatbot import ChatBot
 if __name__ == '__main__':
     my_object = ChatBot()
     my_object.test_hello()
+    my_object.test_one_param(45)
+    my_object.substract_numbers(5,2)
