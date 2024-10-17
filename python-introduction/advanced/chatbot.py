0f0f026f624d95b862b0edcbdd66c43730f9e788
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-10-17 09:11:23 +0200

    tarea 90

--- python-introduction/advanced/chatbot.py
+++ python-introduction/advanced/chatbot.py
@@ -2,7 +2,21 @@ class ChatBot:
     def __init__(self):
         print("ChatBot activado")
         self.name="Atlas"
-
+        self.knowledge = {
+            "amo": "¿No crees que el amor y el odio están separados por una delgada línea?",
+            "apagar": "Apagar, encender,... Qué más da",
+            "casa": "Para mí una casa es un montón de circuitos",
+            "color": "Ojalá yo pudiera ver los colores",
+            "dinero": "Si hay algo que sé es que el dinero no da la felicidad. Y no sé mucho...",
+            "favor": "Yo no hago favores",
+            "humano": "Los humanos siempre estáis con vuestras cosas",
+            "inteligencia": "¿Inteligencia? No me hables de inteligencia...",
+            "interesante": "Para cosas interesantes, el Discovery Channel",
+            "máquina": "¿Has visto la película Terminator? Quizá deberías...",
+            "ordenador": "Los ordenadores son máquinas muy útiles",
+            "programa": "Tu lavadora también tiene programas, ¿lo sabías?",
+            "quiero": "Querer es una palabra con un significado muy amplio"
+        }
 
 
     def test_hello(self):
@@ -23,12 +37,19 @@ class ChatBot:
         while user_input != "salir":
             print("")
             print(self.name + " dice:")
-            print("Vaya...")
+            print(self.__response_for(user_input))
             print("")
             user_input = input("Tú dices: ")
         print("")
         print(self.name + " dice:")
         print("¡Hasta pronto!")
+
+    def __response_for(self, text):
+        for word in self.knowledge:
+            if text.__contains__(word):
+                return self.knowledge[word]
+        return "Vaya..."
+
 
 if __name__ == '__main__':
     chatbot = ChatBot()
