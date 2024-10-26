c995e575a38a4a45c95c142375a0ded277cd0099
Rubén Montero <ruben.montero@fpcoruna.afundacion.org>
2024-10-26 12:13:45 +0200

    Added initial files

---
+++ python-django/tests/context.py
@@ -0,0 +1,11 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+PROJECT_ROOT = '..'
+SIMPLE_API_ROOT = '../apis/SimpleAPI/'
+WALL_API_ROOT = '../apis/WallAPI/'
+DASHBOARD_API_ROOT = '../apis/DashboardAPI/'
+
+
+
