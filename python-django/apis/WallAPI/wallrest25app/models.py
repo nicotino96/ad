215f20ea2b1359a909593e81683ac730174c8ee1
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-11-05 12:17:03 +0100

    Tarea 145

--- python-django/apis/WallAPI/wallrest25app/models.py
+++ python-django/apis/WallAPI/wallrest25app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Entry(models.Model):
+    title = models.CharField(max_length=140)
+    content = models.CharField(max_length=5500)
+    publication_date = models.DateTimeField(auto_now=True)
