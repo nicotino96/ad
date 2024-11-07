699fab5d509ecba8b3452c453e0503d4b5f5f87d
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-11-07 12:35:49 +0100

    148

--- python-django/apis/WallAPI/WallAPI/settings.py
+++ python-django/apis/WallAPI/WallAPI/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'wallrest25app.apps.Wallrest25AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
