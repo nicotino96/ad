1505da10f2639e6b82025fc0f6cbddb445a25627
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-11-08 12:51:51 +0100

    tarea 159

---
+++ python-django/apis/DashboardAPI/dashboard25app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Dashboard25AppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dashboard25app'
---
+++ python-django/apis/DashboardAPI/dashboard25app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Dashboard(models.Model):
+    title = models.CharField(max_length=140)
+    summary = models.CharField(max_length=5500)
+class Question(models.Model):
+    dashboard = models.ForeignKey(Dashboard,on_delete=models.CASCADE)
+    title = models.CharField(max_length=140)
+    summary = models.CharField(max_length=5500)
+    publication_date = models.DateTimeField(auto_now=True)
+class Answer(models.Model):
+    question = models.ForeignKey(Question,on_delete=models.CASCADE)
+    description = models.CharField(max_length=5500)
+    publication_date = models.DateTimeField(auto_now=True)
+
+# Create your models here.
