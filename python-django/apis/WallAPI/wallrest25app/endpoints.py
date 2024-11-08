3c0cc4fa7f59a2571f0ebf9f2efcbca8ff461e29
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-11-08 10:29:57 +0100

    tarea 153

--- python-django/apis/WallAPI/wallrest25app/endpoints.py
+++ python-django/apis/WallAPI/wallrest25app/endpoints.py
@@ -25,13 +25,28 @@ def all_entries(request):
         return JsonResponse({"it_was_ok": True}, status=201)
     else:
         return JsonResponse({"error": "HTTP method not supported"}, status=405)
+@csrf_exempt
 def entry_comments(request, path_param_id):
-    if request.method != "GET":
+    if request.method == "GET":
+        comments = Comment.objects.filter(entry=path_param_id)
+        json_response = []
+        for row in comments:
+            json_response.append(row.to_json())
+        return JsonResponse(json_response, safe=False)
+    elif request.method == "POST":
+        client_json = json.loads(request.body)
+        client_content = client_json.get("new_content", None)
+        if client_content is None:
+            return JsonResponse({"error": "Missing new_content"}, status=400)
+            # Creamos una nueva instancia de Comment
+            # Nótese pasamos entry_id=... en vez de entry=...
+            # para indicar que nos referimos a la clave primaria (id)
+            # del atributo. Esto se hace así para los models.ForeignKey
+        new_comment = Comment(content=client_content, entry_id=path_param_id)
+        new_comment.save()
+        return JsonResponse({"new_comment_created": True}, status=201)
+    else:
         return JsonResponse({"error": "HTTP method not supported"}, status=405)
-    comments = Comment.objects.filter(entry=path_param_id)
-    json_response = []
-    for row in comments:
-        json_response.append(row.to_json())
-    return JsonResponse(json_response, safe=False)
+
 
 
