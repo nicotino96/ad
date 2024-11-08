71aa4684769b39210ceadae071f3eadf2a788890
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-11-08 20:57:41 +0100

    tareas 160 y 161 en progreso

--- python-django/apis/DashboardAPI/DashboardAPI/settings.py
+++ python-django/apis/DashboardAPI/DashboardAPI/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'dashboard25app.apps.Dashboard25AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ python-django/apis/DashboardAPI/dashboard25app/endpoints.py
@@ -0,0 +1,55 @@
+import json
+
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import Dashboard
+from .models import Question
+
+def all_dashboards(request):
+    if request.method != "GET":
+        return JsonResponse({"error": "HTTP method not supported"}, status=405)
+    all_rows = Dashboard.objects.all()
+    json_response = []
+    for row in all_rows:
+        json_response.append(row.to_json())
+    return JsonResponse(json_response, safe=False)
+@csrf_exempt
+def questions_from_dashboard(request, path_param_id):
+    if request.method == "GET":
+        before = request.GET.get("before", None)
+        size = request.GET.get("size", None)
+        if size is None:
+            try:
+                size = int(size)
+            except ValueError:
+                return JsonResponse({"error": "Wrong size parameter"}, status=400)
+            if before is None:
+                questions = Question.objects.filter(dashboard=path_param_id).order_by('-publication_date')
+            else:
+                questions = Question.objects.filter(dashboard=path_param_id).filter(
+                    publication_date__lt=before).order_by("-publication_date")
+        else:
+            if before is None:
+                questions = Question.objects.filter(dashboard=path_param_id).order_by('-publication_date')[:size]
+            else:
+                questions = Question.objects.filter(dashboard=path_param_id).filter(
+                    publication_date__lt=before).order_by("-publication_date")[:size]
+        json_response = []
+        for row in questions:
+            json_response.append(row.to_json())
+        return JsonResponse(json_response, safe=False)
+
+    elif request.method == "POST":
+        client_json = json.loads(request.body)
+        client_title = client_json.get("title", None)
+        client_summary = client_json.get("summary", None)
+
+        if client_title is None or client_summary is None:
+            return JsonResponse({"error": "Missing summary or title in request body"}, status=400)
+        new_question = Question(title=client_title, summary=client_summary, dashboard_id=path_param_id)
+        new_question.save()
+        return JsonResponse({"success": True}, status=201)
+    else:
+        return JsonResponse({"error": "HTTP method not supported"}, status=405)
+
