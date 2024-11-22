d1fa9104e858182677b01d83c523d6c575d4f790
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-11-22 13:29:46 +0100

    171 terminada

--- python-sessions/apis/IdeAPI/idearest25app/models.py
+++ python-sessions/apis/IdeAPI/idearest25app/models.py
@@ -5,3 +5,7 @@ class CustomUser(models.Model):
     username = models.CharField(max_length=200)
     encrypted_password = models.CharField(max_length=120)
 
+class UserSession(models.Model):
+    creator = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    token = models.CharField(unique=True, max_length=35)
+
