1159b54c26458d8a64a3db6829043ffb555c0800
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-11-22 10:48:20 +0100

    tarea 167

--- python-sessions/apis/IdeAPI/IdeAPI/settings.py
+++ python-sessions/apis/IdeAPI/IdeAPI/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'idearest25app.apps.Idearest25AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ python-sessions/apis/IdeAPI/idearest25app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Idearest25AppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'idearest25app'
