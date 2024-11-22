ddc51ac76aa5b7770c07da0f10c780c7a2d898c1
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-11-22 10:58:38 +0100

    tarea 168

--- python-sessions/apis/IdeAPI/idearest25app/models.py
+++ python-sessions/apis/IdeAPI/idearest25app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class CustomUser(models.Model):
+    e_mail = models.CharField(max_length=290, unique=True)
+    username = models.CharField(max_length=200)
+    encrypted_password = models.CharField(max_length=120)
+
