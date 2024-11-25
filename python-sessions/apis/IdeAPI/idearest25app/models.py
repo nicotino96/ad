0eab0451d28b3574af96022b8a89e5fccffb8617
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-11-25 18:26:39 +0100

    174 terminada

--- python-sessions/apis/IdeAPI/idearest25app/models.py
+++ python-sessions/apis/IdeAPI/idearest25app/models.py
@@ -9,3 +9,18 @@ class UserSession(models.Model):
     creator = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     token = models.CharField(unique=True, max_length=35)
 
+class Category(models.Model):
+    title = models.CharField(max_length=60, unique=True)
+    def __str__(self):
+        return self.title
+
+class Idea(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=2400)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    content = models.CharField(max_length=1400)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    idea = models.ForeignKey(Idea, on_delete=models.CASCADE)
